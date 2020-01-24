8324c9af3457461f42db716bbd4abfc35a1bb82c
Matthias Bauw <matthias.bauw@student.vives.be>
2020-01-24 14:50:23 +0100

    Add project files.

---
+++ Contactmoment-24012020-2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Contactmoment_24012020_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
